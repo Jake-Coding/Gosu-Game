8cc3249b1b0b510a2d66770c7dbbde6f74449cec
Jake Bridge <jakebridge2005@gmail.com>
2019-10-11 09:42:31 -0600

    Add star collection

--- gosu_game.rb
+++ gosu_game.rb
@@ -1,4 +1,27 @@
 require 'gosu'
+module ZOrder
+    BACKGROUND, STARS, PLAYER, UI = *0..3
+end
+
+class Star 
+    attr_reader :x, :y
+
+    def initialize(animation)
+        @animation = animation
+        @color = Gosu::Color::BLACK.dup
+        @color.red = rand(256-40) + 40
+        @color.green = rand(256 - 40) + 40
+        @color.blue = rand(256-40) + 40
+        @x = rand * 640
+        @y = rand * 480
+    end
+
+    def draw
+        img = @animation[Gosu.milliseconds / 100 % @animation.size]
+        img.draw(@x - img.width / 2.0, @y - img.height / 2.0, ZOrder::STARS, 1, 1, @color, :add)
+    end
+end
+
 class Player
     def initialize
         @image = Gosu::Image.new("media/starfighter.bmp")
@@ -32,7 +55,15 @@ class Player
     def draw
         @image.draw_rot(@x, @y, 1, @angle)
     end
+    def score
+        @score
+    end
+
+    def collect_stars(stars)
+        stars.reject! {|star| Gosu.distance(@x, @y, star.x, star.y) < 35 }
+    end
 end
+
 class Tutorial < Gosu::Window
     def initialize
         super 640, 480
@@ -41,6 +72,9 @@ class Tutorial < Gosu::Window
         @background_image = Gosu::Image.new("media/space.png", :tileable => true)
         @player = Player.new
         @player.warp(320, 240)
+
+        @star_anim = Gosu::Image.load_tiles("media/star.png", 25, 25)
+        @stars = Array.new
     end
 
     def update
@@ -54,11 +88,17 @@ class Tutorial < Gosu::Window
             @player.accelerate
         end
         @player.move   
+        @player.collect_stars(@stars)
+
+        if rand(100) < 4 and @stars.size < 25
+            @stars.push(Star.new(@star_anim))
+        end
     end
 
     def draw
         @player.draw
         @background_image.draw(0,0,0)
+        @stars.each {|star| star.draw}
     end
     def button_down(id)
         if id == Gosu::KB_ESCAPE
