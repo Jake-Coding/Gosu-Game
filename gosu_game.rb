f410a50a23313a9a2bae6368133b4a0684f14e07
Jake Bridge <jakebridge2005@gmail.com>
2019-10-11 09:51:46 -0600

    Score System & Beep

--- gosu_game.rb
+++ gosu_game.rb
@@ -23,10 +23,12 @@ class Star
 end
 
 class Player
+    attr_reader :score
     def initialize
         @image = Gosu::Image.new("media/starfighter.bmp")
         @x = @y = @vel_x = @vel_y = @angle = 0.0
         @score = 0
+        @beep = Gosu::Sample.new("media/beep.wav")
     end
 
     def warp(x,y)
@@ -60,7 +62,15 @@ class Player
     end
 
     def collect_stars(stars)
-        stars.reject! {|star| Gosu.distance(@x, @y, star.x, star.y) < 35 }
+        stars.reject! do |star|
+            if Gosu.distance(@x, @y, star.x, star.y) < 35
+                @score += 10
+                @beep.play
+                true
+            else
+                false
+            end
+        end
     end
 end
 
@@ -75,6 +85,7 @@ class Tutorial < Gosu::Window
 
         @star_anim = Gosu::Image.load_tiles("media/star.png", 25, 25)
         @stars = Array.new
+        @font = Gosu::Font.new(20)
     end
 
     def update
@@ -97,8 +108,9 @@ class Tutorial < Gosu::Window
 
     def draw
         @player.draw
-        @background_image.draw(0,0,0)
+        @background_image.draw(0,0, ZOrder::BACKGROUND)
         @stars.each {|star| star.draw}
+        @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
     end
     def button_down(id)
         if id == Gosu::KB_ESCAPE
