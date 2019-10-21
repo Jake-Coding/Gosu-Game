4a4b996f840c8b9e5d47775e77d75ac7d339400b
Jake Bridge <jakebridge2005@gmail.com>
2019-10-21 09:34:08 -0600

    It went to hell. Oh noes. Hard Reset

--- gosu_game.rb
+++ gosu_game.rb
@@ -1,34 +1,9 @@
 require 'gosu'
-module ZOrder
-    BACKGROUND, STARS, PLAYER, UI = *0..3
-end
-
-class Star 
-    attr_reader :x, :y
-
-    def initialize(animation)
-        @animation = animation
-        @color = Gosu::Color::BLACK.dup
-        @color.red = rand(256-40) + 40
-        @color.green = rand(256 - 40) + 40
-        @color.blue = rand(256-40) + 40
-        @x = rand * 640
-        @y = rand * 480
-    end
-
-    def draw
-        img = @animation[Gosu.milliseconds / 100 % @animation.size]
-        img.draw(@x - img.width / 2.0, @y - img.height / 2.0, ZOrder::STARS, 1, 1, @color, :add)
-    end
-end
-
 class Player
-    attr_reader :score
     def initialize
         @image = Gosu::Image.new("media/starfighter.bmp")
         @x = @y = @vel_x = @vel_y = @angle = 0.0
         @score = 0
-        @beep = Gosu::Sample.new("media/beep.wav")
     end
 
     def warp(x,y)
@@ -57,23 +32,7 @@ class Player
     def draw
         @image.draw_rot(@x, @y, 1, @angle)
     end
-    def score
-        @score
-    end
-
-    def collect_stars(stars)
-        stars.reject! do |star|
-            if Gosu.distance(@x, @y, star.x, star.y) < 35
-                @score += 10
-                @beep.play
-                true
-            else
-                false
-            end
-        end
-    end
 end
-
 class Tutorial < Gosu::Window
     def initialize
         super 640, 480
@@ -82,10 +41,6 @@ class Tutorial < Gosu::Window
         @background_image = Gosu::Image.new("media/space.png", :tileable => true)
         @player = Player.new
         @player.warp(320, 240)
-
-        @star_anim = Gosu::Image.load_tiles("media/star.png", 25, 25)
-        @stars = Array.new
-        @font = Gosu::Font.new(20)
     end
 
     def update
@@ -99,18 +54,11 @@ class Tutorial < Gosu::Window
             @player.accelerate
         end
         @player.move   
-        @player.collect_stars(@stars)
-
-        if rand(100) < 4 and @stars.size < 25
-            @stars.push(Star.new(@star_anim))
-        end
     end
 
     def draw
         @player.draw
-        @background_image.draw(0,0, ZOrder::BACKGROUND)
-        @stars.each {|star| star.draw}
-        @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+        @background_image.draw(0,0,0)
     end
     def button_down(id)
         if id == Gosu::KB_ESCAPE
